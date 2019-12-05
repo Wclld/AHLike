f66eaf46dfcd72731cdec80b1f420ebc057639f5
Wclld <gabalisyura@gmail.com>
2019-12-05 17:46:32 +0300

    Adding room management system

---
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace AHLike.Rooms
+{
+    public class RoomManager : MonoBehaviour
+    {
+        public event Action<RoomInfo> OnRoomLoaded;
+        public event Action OnRoomUnloaded;
+
+        private GameObject _currentMap;
+
+
+        public void LoadRoom(GameObject roomPrefab)
+        {
+            _currentMap = Instantiate(roomPrefab,Vector3.zero,Quaternion.identity,transform);
+            var roomInfo = RoomInfo.GetRoomInfo(_currentMap);
+            OnRoomLoaded?.Invoke(roomInfo);
+        }
+        
+
+        public void UnloadLastRoom()
+        {
+            if(_currentMap != null)
+            {
+                Destroy(_currentMap);                
+            }
+            OnRoomUnloaded?.Invoke();
+        }
+    }
+}
