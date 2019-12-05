49d00e2ec85d374c50bf3c9edec37978d1583136
Wclld <gabalisyura@gmail.com>
2019-12-05 21:11:48 +0300

    Created player manager and scriptable for heroes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,25 +1,50 @@
 using UnityEngine;
 using AHLike.Movement;
+using AHLike.Data;
 
 namespace AHLike.Player
 {
+    [RequireComponent(typeof(Rigidbody))]
     internal class PlayerController : MonoBehaviour 
     {
-        [SerializeField] MovementController _movement;
+        [SerializeField] float _moveSpeed = 300;
+        private Rigidbody _rigidbody;
+        private MovementInput _input;
+        private Vector3 _movement;
+        private Quaternion _direction;
 
         private void Start() 
         {
-            GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.Interpolate;
+            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+            #if UNITY_EDITOR
+            _input = new KeyboarInput();        
+#else 
+            _input = new JoystickInput();
+#endif    
+            _input.OnInputBegin += ()=> Debug.Log("On input Begin");
+            _input.OnInputEnded += ()=> Debug.Log("On input Ended");
         }
-        
+
         private void Update() 
         {
-            _movement.Update();   
+            _movement = _input.GetMovement();
+            _movement *= _moveSpeed * Time.deltaTime;
+            _direction.eulerAngles = _movement.normalized; 
         }    
 
         private void LateUpdate() 
         {
-            _movement.LateUpdate();    
+            _rigidbody.velocity = _movement;
+            if(_movement != Vector3.zero)
+            {
+                _rigidbody.transform.rotation = Quaternion.LookRotation(_movement,Vector3.up);
+            }    
+        }
+
+        private void SetSpeed(float speed)
+        {
+            _moveSpeed = speed;
         }
     }
 }
